d31dc9f90ce99b12595290727813998aa498e80e
Gohil Vishal <gohilvd04@gmail.com>
2022-08-30 16:58:50 +0530

    first commit

---
+++ script.js
@@ -0,0 +1,23 @@
+const tabs = document.querySelector(".tabs");
+const btns = document.querySelectorAll(".btn");
+const articles = document.querySelectorAll(".content");
+
+
+tabs.addEventListener("click",(event)=>{
+    const id = event.target.dataset.id;
+    if (id) {
+        btns.forEach(btn=>{
+            btn.classList.remove("live");
+        });
+        event.target.classList.add("live");
+
+        articles.forEach(article=>{
+            article.classList.remove("live");
+        });
+
+        const element = document.getElementById(id);
+        element.classList.add("live");
+    }
+
+
+})
